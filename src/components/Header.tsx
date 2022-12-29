a2fec3a5d021689d0e3e604a12ed8159c39dcb38
Tony Filgueiras <ant.mosquera@yahoo.com.br>
2022-12-29 19:25:10 -0300

    translating app

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import style from "../css/Header.module.css"
 import usaLogo from "../img/eua.png"
 import brlLogo from "../img/brasil.png"
 import { NavLink, useLocation } from 'react-router-dom'
+import { UserContext } from '../UserContext'
 
 
 
@@ -10,22 +11,38 @@ export default function Header() {
   const [pathname, setPathname] = React.useState("")
   const location = useLocation()
 
+  const {languageStorage, setLanguageStorage}= React.useContext(UserContext)
+
+
+  type PathnameTitle= {
+    [key: string]: {[key:string]: string}
+  }
+
+  const pathnameTitle : PathnameTitle={
+    speed: {en: "Speed", pt: "Velocidade"},
+    temperature: {en: "Temperature", pt: "Temperatura"},
+    weight: {en: "Weight", pt: "Peso"},
+  }
+
+
   React.useEffect(() => {
-    setPathname(location.pathname.replace("/", "").charAt(0).toUpperCase() +  location.pathname.replace("/", "").slice(1))
+    setPathname(location.pathname.replace("/", ""))
   },[location]);
 
   return (
     <header className={style.header}>
         <NavLink to="/">
-          <h1>Tony's Conversions {pathname? <>-</> : <></>} {pathname}</h1>
+          <h1>Tony's Conversions {pathname? <>- {pathnameTitle[pathname as keyof PathnameTitle][languageStorage as keyof PathnameTitle]}</>: null}</h1>
         </NavLink>
         <nav>
             <ul>
-                <li><img src={usaLogo} alt='USA' className={style.flag}/>En</li>
-                <li><img src={brlLogo} alt='BRL' className={style.flag}/>Pt</li>
+                <li onClick={()=>setLanguageStorage("en")}><img src={usaLogo} alt='USA' className={style.flag}/>En</li>
+                <li onClick={()=>setLanguageStorage("pt")}><img src={brlLogo} alt='BRL' className={style.flag}/>Pt</li>
             </ul>
         </nav>
     </header>
     
   )
 }
+
+// setPathname(location.pathname.replace("/", "").charAt(0).toUpperCase() +  location.pathname.replace("/", "").slice(1))
--- src/components/HomeView.tsx
+++ src/components/HomeView.tsx
@@ -4,24 +4,33 @@ import peso from "../img/peso.svg"
 import temp from "../img/temp.png"
 import vel from "../img/vel.png"
 import { NavLink } from 'react-router-dom'
+import { UserContext } from '../UserContext'
 
-// type Props = {}
+type ContainerTitle = {
+  [key:string]: string  
+}
 
-const list = [
+type Options = {
+  containerTitle: ContainerTitle,
+  imageSource: any,
+  imageAlt: string,
+  containerRoute: string
+}
+const options : Options[] = [
   {
-    containerTitle: "Weight Conversion",
+    containerTitle: {en: "Weight Conversion", pt: "Conversão de Peso"},
     imageSource: peso,
     imageAlt: "Weight image",
     containerRoute: "/weight",
   },
   {
-    containerTitle: "Temperature Conversion",
+    containerTitle: {en: "Temperature Conversion", pt: "Conversão de Temperatura"},
     imageSource: temp,
     imageAlt: "Temperature image",
     containerRoute: "/temperature",
   },
   {
-    containerTitle: "Speed Conversion",
+    containerTitle: {en: "Speed Conversion", pt: "Conversão de Velocidade"},
     imageSource: vel,
     imageAlt: "Speed image",
     containerRoute: "/speed",
@@ -31,13 +40,16 @@ const list = [
 ]
 
 export default function HomeView() {
+
+  const{languageStorage}= React.useContext(UserContext)
+
   return (
     <ul className={style.ul}>
-        {list.map((item) => 
-          <NavLink to={item.containerRoute} key={item.containerTitle}>
+        {options.map((item) => 
+          <NavLink to={item.containerRoute} key={item.containerTitle['pt']}>
             <li className={style.li} >
           
-              <h1 className={style.h1}>{item.containerTitle}</h1>
+              <h1 className={style.h1}>{item.containerTitle[languageStorage as keyof ContainerTitle]}</h1>
               <img src={item.imageSource} alt={item.imageAlt} className={style.image}/>
             </li>
           </NavLink>
